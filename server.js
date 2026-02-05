4c33154cefa31ede7002f47346e1586c6224f76c
kl_ <clwilson2@students.wcpss.net>
2026-02-04 20:10:12 -0500

    Update server.js update one, movement n stuff

--- server.js
+++ server.js
@@ -6,29 +6,41 @@ const io = require('socket.io')(http, { cors: { origin: "*" } });
 app.use(express.static('public'));
 
 let players = {};
+let trees = [];
+
+// Generate trees on server start
+for (let i = 0; i < 15; i++) {
+    trees.push({
+        id: i,
+        x: Math.random() * 40 - 20,
+        z: Math.random() * 40 - 20
+    });
+}
 
 io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
+    players[socket.id] = { x: 0, y: 1.6, z: 0, username: "Guest", color: Math.floor(Math.random()*16777215).toString(16) };
 
-    // Create a new player with a random color
-    players[socket.id] = {
-        x: 0, y: 0, z: 0,
-        username: "Guest",
-        color: Math.floor(Math.random()*16777215).toString(16)
-    };
+    // Send existing trees to new player
+    socket.emit('init-trees', trees);
 
     socket.on('join', (data) => {
         players[socket.id].username = data.username || "Guest";
         io.emit('update-players', players);
     });
 
-    socket.on('move', (position) => {
+    socket.on('move', (pos) => {
         if (players[socket.id]) {
-            players[socket.id].x = position.x;
-            players[socket.id].y = position.y;
-            players[socket.id].z = position.z;
-            socket.broadcast.emit('player-moved', { id: socket.id, pos: position });
+            players[socket.id].x = pos.x;
+            players[socket.id].y = pos.y;
+            players[socket.id].z = pos.z;
+            socket.broadcast.emit('player-moved', { id: socket.id, pos });
         }
+    });
+
+    socket.on('click-tree', (treeId) => {
+        // Remove tree and tell everyone
+        trees = trees.filter(t => t.id !== treeId);
+        io.emit('tree-removed', treeId);
     });
 
     socket.on('disconnect', () => {
