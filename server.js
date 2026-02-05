d2b61cfc3646ffe5fb9ba3f00b49dbba605bee66
kl_ <clwilson2@students.wcpss.net>
2026-02-04 20:35:03 -0500

    Update server.js rotation

--- server.js
+++ server.js
@@ -1,44 +1,45 @@
 const express = require('express');
-const path = require('path'); // Add this for file paths
+const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { cors: { origin: "*" } });
 
-// This line tells Express to look INSIDE the 'public' folder for index.html
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Fallback: If someone goes to the root, send them index.html explicitly
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 let players = {};
 let trees = [];
 
-// Generate trees
-for (let i = 0; i < 15; i++) {
+// Initial tree spawn
+for (let i = 0; i < 20; i++) {
     trees.push({
         id: i,
-        x: Math.random() * 40 - 20,
-        z: Math.random() * 40 - 20
+        x: Math.random() * 60 - 30,
+        z: Math.random() * 60 - 30
     });
 }
 
 io.on('connection', (socket) => {
-    players[socket.id] = { x: 0, y: 1.6, z: 0, username: "Guest", color: Math.floor(Math.random()*16777215).toString(16) };
+    // Added 'ry' for Rotation Y
+    players[socket.id] = { 
+        x: 0, y: 1.6, z: 0, ry: 0, 
+        username: "Guest", 
+        color: Math.floor(Math.random()*16777215).toString(16) 
+    };
+
     socket.emit('init-trees', trees);
 
     socket.on('join', (data) => {
-        players[socket.id].username = data.username || "Guest";
+        if(players[socket.id]) players[socket.id].username = data.username || "Guest";
         io.emit('update-players', players);
     });
 
-    socket.on('move', (pos) => {
+    socket.on('move', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = pos.x;
-            players[socket.id].y = pos.y;
-            players[socket.id].z = pos.z;
-            socket.broadcast.emit('player-moved', { id: socket.id, pos });
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            players[socket.id].z = data.z;
+            players[socket.id].ry = data.ry; 
+            socket.broadcast.emit('player-moved', { id: socket.id, pos: data });
         }
     });
 
